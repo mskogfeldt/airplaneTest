3b419eeadc3757efbb5ee7543ba3bcfc246db501
mskogfeldt <mskogfeldt@hotmail.com>
2019-10-17 16:53:42 +0200

    some stuff to change things in maps

--- dao-impl/src/main/java/se/mskogfeldt/dao/impl/TripDaoImpl.java
+++ dao-impl/src/main/java/se/mskogfeldt/dao/impl/TripDaoImpl.java
@@ -40,14 +40,53 @@ public void create(Account account) {
     public Optional<TripEntity> readTripEntity(String id) {
         return Optional.ofNullable(trips.get(id));
 
-
       /*  Return trips.values().stream().filter(TripEntity -> TripEntity.getId().equalsIgnoreCase(id)).collect(Collectors.toSet());
 
         //List<FoodEntity> orderdFood = bookedFood.values().stream().filter(customerEntity -> TripEntity.getSsn().equalsIgnoreCase(ssn)).collect(Collectors.toSet());
         */
     }
 
+    public List returnBookdedFood(Map<String, List<String>>data,Integer i){
+        if(data.containsKey(i))
+            return data.get(i).stream().collect(Collectors.toList());
+        return null;
+    }public void delitingValues(Map<Integer, List<String>>data,Integer i,List<String> values){
+        System.out.println("the value of"+data.get(i)+"is deleted");
+        data.remove(i);
+        data.put(i,values);
+        System.out.println("the new map value"+data.get(i));}
 }
 
+}
 
+/*
+    public class MapDemo {
+       public static void main(String[] args) {
+           MapDemo demo=new MapDemo();
+           List<String> list = new ArrayList<>();        Map<Integer, List<String>> data = new HashMap<>();
+           for (int i = 1; i < 10; i++) {
+               list.add("name" + i);
+               data.put(i,list);
+           }
+           System.out.println(list);
+           System.out.println("Data: " + data);
+           System.out.println("DataEntrySet: "+ data.entrySet());
+           System.out.println("Data.values: " + data.values());*/
+//System.out.println(demo.returnSmallList(data));
+// System.out.println(demo.returnSmallList2(data, 11));
+    /*List<String> matslist = Arrays.asList("mats", "biniam");
+       demo.delitingValues(data,4,matslist);
+               }public List returnSmallList(Map<Integer, List<String>>data){return data.values().stream().collect(Collectors.toList());}
+public List returnSmallList2(Map<Integer, List<String>>data,Integer i){
+        if(data.containsKey(i))
+        return data.get(i).stream().collect(Collectors.toList());
+        return null;
+        }public void delitingValues(Map<Integer, List<String>>data,Integer i,List<String> values){
+        System.out.println("the value of"+data.get(i)+"is deleted");
+        data.remove(i);
+        data.put(i,values);
+        System.out.println("the new map value"+data.get(i));}
+        }
+
+        */
 
